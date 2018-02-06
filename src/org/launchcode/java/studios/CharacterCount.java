2dac8abe24a4a1c27c67bc8c3f2b20fec6a4dd4c
Victor <victor.n.tran@outlook.com>
2018-02-06 16:50:24 -0500

    Edited to change complete bonus mission

--- src/org/launchcode/java/studios/CharacterCount.java
+++ src/org/launchcode/java/studios/CharacterCount.java
@@ -7,23 +7,20 @@ public class CharacterCount {
 
     public static void main(String[] args) {
 
-        String search = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                "Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis " +
-                "augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet " +
-                "suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, " +
-                "volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
-
-        char[] searchArray = search.toCharArray();
+        System.out.println(args.toString());
+        char[] searchArray = args.toString().toLowerCase().toCharArray();
 
         Integer start = 1;
         HashMap<Character, Integer> characters = new HashMap<>();
 
         for (int i = 0; i < searchArray.length; i++) {
-            if (!characters.containsKey(searchArray[i])) {
-                Integer initialValue;
-                characters.put(searchArray[i], initialValue = start);
-            } else {
-                characters.replace(searchArray[i], characters.get(searchArray[i]),characters.get(searchArray[i]) + 1);
+            if (Character.isLetter(searchArray[i])) {
+                if (!characters.containsKey(searchArray[i])) {
+                    Integer initialValue;
+                    characters.put(searchArray[i], initialValue = start);
+                } else {
+                    characters.replace(searchArray[i], characters.get(searchArray[i]), characters.get(searchArray[i]) + 1);
+                }
             }
         }
 
